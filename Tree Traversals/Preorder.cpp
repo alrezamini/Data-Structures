b02e66fabf1d93d5dd5b9f0294f792ebc464b216
Alireza Amini <alrezamini@yahoo.com>
2021-09-10 14:44:31 +0430

    Removed Some Nodes

--- Tree Traversals/Preorder.cpp
+++ Tree Traversals/Preorder.cpp
@@ -15,10 +15,8 @@ int main(){
     TreeNode *root=new TreeNode(1);
     root->left=new TreeNode(2);
     root->right=new TreeNode(3);
-    root->left->left=new TreeNode(5);
-    root->left->right=new TreeNode(6);
-    root->right->left=new TreeNode(7);
-    root->right->right=new TreeNode(8);
+    root->left->left=new TreeNode(4);
+    root->left->right=new TreeNode(5);
 
     TreeTraversals object;
     object.Preorder(root);
